a75c58e9abbe0705cc873bbe7bcee384ba8a1dea
Chuxi Wang <wangchuxi94@gmail.com>
2021-12-13 22:23:05 -0800

    user update modal

--- client/src/components/ingame/InGamePage.js
+++ client/src/components/ingame/InGamePage.js
@@ -4,6 +4,7 @@ import * as SockJS from 'sockjs-client';
 import { Stomp } from '@stomp/stompjs';
 import {Button, message, Row, Col, Card, Divider, Modal, Input } from 'antd';
 import axios from "axios";
+import { MoreOutlined } from '@ant-design/icons';
 import 'react-chatbox-component/dist/style.css';
 import { ChatBox } from 'react-chatbox-component';
 import rollADie from 'roll-a-die';
@@ -16,6 +17,16 @@ const InGamePage = (props) => {
     const [ players, setPlayers ] = useState([]);
     const [ isDiceModalVisible, setDiceModalVisible ] = useState(false);
     const [ diceInput, setDiceInput ] = useState('');
+
+    const [ playerUpdateModalVisible, setPlayerUpdateModalVisible ] = useState(false);
+
+    const [ selectedPlayer, setSelectedPlayer ] = useState({});
+
+
+    const [ hp, setHp ] = useState('');
+    const [ sanity, setSanity ] = useState('');
+    const [ mp, setMp ] = useState('');
+    const [ luck, setLuck ] = useState('');
 
     
     const [stompClient, setClient] = useState(undefined);
@@ -48,18 +59,24 @@ const InGamePage = (props) => {
         }
         if (stompClient) {
           stompClient.connect(header, function (frame) {   
-            stompClient.subscribe('/topic/message/29', function (greeting) {
-                combinedMessages.push({
-                "text": JSON.parse(greeting.body).msgBody,
-                "id": combinedMessages.length === 0 ? "1" : (combinedMessages?.[combinedMessages.length - 1]['id'] + 1).toString(),
-                "sender": {
-                  "name": "Ironman",
-                  "uid": "user1",
-                  "avatar": "https://data.cometchat.com/assets/images/avatars/ironman.png",
-                },
-              });
-              setMessages([...combinedMessages])
+            stompClient.subscribe('/topic/characters/29', function (greeting) {
+              const updatedPlayer = JSON.parse(greeting.body);
+              const index = players.findIndex((player) => player['id'].toString() === updatedPlayer['id'].toString());
+              setPlayers(players.slice(0, index).concat([updatedPlayer], players.slice(index + 1)));
             });
+
+            stompClient.subscribe('/topic/message/29', function (greeting) {
+              combinedMessages.push({
+              "text": JSON.parse(greeting.body).msgBody,
+              "id": combinedMessages.length === 0 ? "1" : (combinedMessages?.[combinedMessages.length - 1]['id'] + 1).toString(),
+              "sender": {
+                "name": "Ironman",
+                "uid": "user1",
+                "avatar": "https://data.cometchat.com/assets/images/avatars/ironman.png",
+              },
+            });
+            setMessages([...combinedMessages])
+          });
           });
     
     
@@ -86,9 +103,26 @@ const InGamePage = (props) => {
     const handleDiceRolling = () => {
       setDiceModalVisible(false);
       const total = droll.roll(diceInput).total;
-      console.log(document.getElementById('dice-roll'));
       rollADie({element: document.getElementById('dice-roll'), numberOfDice:1, callback:() => {}});
       setDiceInput('');
+    }
+
+    const onUpdatePlayer = (plId) => {
+      setPlayerUpdateModalVisible(false);
+      const oldPlayer = players.find((player) => player['id'] === plId);
+      stompClient.send("/app/status/29", {"name": "mialsy"}, JSON.stringify(
+        {'plId' : plId, 'hpChange': hp === ''? oldPlayer['hp']: parseInt(hp), 'mpChange': mp === ''? oldPlayer['mp']: parseInt(mp), 'sanChange': sanity === ''? oldPlayer['sanity']: parseInt(sanity), 'luckChange': luck === ''? oldPlayer['luck']: parseInt(luck)}));     
+      setMp('');
+      setHp('');
+      setSanity('');
+      setLuck('');
+      setSelectedPlayer({});
+    }
+
+    const openPlayerModal = (plId) => {
+      setPlayerUpdateModalVisible(true);
+      const selectedPlayer = players.find((player) => player['id'].toString() === plId.toString());
+      setSelectedPlayer(selectedPlayer);
     }
 
     const handleDiceCancel = () => {
@@ -96,12 +130,35 @@ const InGamePage = (props) => {
       setDiceInput('');
     }
 
+    const onUpdatePlayerModalCancel = () => {
+      setPlayerUpdateModalVisible(false);
+      setSelectedPlayer({});
+      setHp('');
+      setSanity('');
+      setLuck('');
+      setMp('');
+    }
+
     const onSubmit= (msg) => {        
-         stompClient.send("/app/message/29", {"name": "mialsy"}, JSON.stringify({'msgBody' : msg, 'by': '122'}));         
+      stompClient.send("/app/message/29", {"name": "mialsy"}, JSON.stringify({'msgBody' : msg, 'by': '122'}));         
     }
 
     return (
     <> 
+      <Modal title="Player Update" visible={playerUpdateModalVisible} onOk={() => onUpdatePlayer(selectedPlayer["id"])} onCancel={onUpdatePlayerModalCancel} okText="update">
+      <Card title={selectedPlayer?.["name"]} style={{ margin: 10 }}>
+          <p>{`Sex: ${selectedPlayer?.["sex"]}`}</p>
+          <p>{`Occupation: ${selectedPlayer?.["occupation"]}`}</p>
+          <p>{`HP: ${selectedPlayer?.["hp"]}`}</p>
+          <Input placeholder="Input a number" maxLength={5} onChange={event=>{setHp(event.target.value)}} value={hp}/>
+          <p>{`MP: ${selectedPlayer?.["mp"]}`}</p>
+          <Input placeholder="Input a number" maxLength={5} onChange={event=>{setMp(event.target.value)}} value={mp}/>
+          <p>{`Luck: ${selectedPlayer?.["luck"]}`}</p>
+          <Input placeholder="Input a number" maxLength={5} onChange={event=>{setLuck(event.target.value)}} value={luck}/>
+          <p>{`Sanity: ${selectedPlayer?.["sanity"]}`}</p>
+          <Input placeholder="Input a number" maxLength={5} onChange={event=>{setSanity(event.target.value)}} value={sanity}/>
+      </Card>
+      </Modal>
       <Modal title="Dice Modal" visible={isDiceModalVisible} onOk={handleDiceRolling} onCancel={handleDiceCancel}>
         <Input placeholder="Dice input here..." value={diceInput} onChange={(event) => {setDiceInput(event.target.value)}}/>
       </Modal>
@@ -114,14 +171,14 @@ const InGamePage = (props) => {
 
 return !player['isKeeper'] &&
 (<>
-<Card title={player["name"]} style={{ margin: 10 }}>
+<Card title={player["name"]} style={{ margin: 10 }} id={player['id']}>
   <p>{`Sex: ${player["sex"]}`}</p>
   <p>{`Occupation: ${player["occupation"]}`}</p>
   <p>{`HP: ${player["hp"]}`}</p>
   <p>{`MP: ${player["mp"]}`}</p>
   <p>{`Luck: ${player["luck"]}`}</p>
   <p>{`Sanity: ${player["sanity"]}`}</p>
-  <p>{`Description: ${player["description"]}`}</p>
+  <p>{`Description: ${player["description"]}`} <Button type="text" ghost onClick={() => openPlayerModal(player['id'])} icon={<MoreOutlined  />}></Button></p>
 </Card>
 <Divider type="vertical" />
 </>)
--- coc/src/main/java/com/mialsy/coc/controllers/CharacterController.java
+++ coc/src/main/java/com/mialsy/coc/controllers/CharacterController.java
@@ -6,6 +6,7 @@ import com.mialsy.coc.models.Player;
 import com.mialsy.coc.pojos.PlayerPojo;
 import com.mialsy.coc.utils.ErrorUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -16,17 +17,18 @@ public class CharacterController {
     @Autowired
     PlayerRepository playerRepository;
 
-    @MessageMapping("/status")
-    @SendTo("/topic/characters")
-    public PlayerPojo changeStatus(StatusMsg message) {
+    @MessageMapping("/status/{gameId}")
+    @SendTo("/topic/characters/{gameId}")
+    public Player changeStatus(StatusMsg message, @DestinationVariable String gameId) {
         // update in db
+
         Player player = playerRepository.findById(message.getPlId())
                 .orElseThrow(() -> ErrorUtils.getObjectNotFoundException(Player.class.getName(), message.getPlId()));
-        player.setHp(player.getHp() + message.getHpChange());
-        player.setMp(player.getMp() + message.getMpChange());
-        player.setLuck(player.getLuck() + message.getLuckChange());
-        player.setSanity(player.getSanity() + message.getSanChange());
-        return playerRepository.save(player).toPojo();
+        player.setHp(message.getHpChange());
+        player.setMp(message.getMpChange());
+        player.setLuck(message.getLuckChange());
+        player.setSanity(message.getSanChange());
+        return playerRepository.save(player);
     }
 
 }
